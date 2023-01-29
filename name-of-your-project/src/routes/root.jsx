9f36020877dba347ce9b95aba189b1d53cc68f9f
Seth Weiss <sweisss@gmail.com>
2023-01-28 23:11:00 -0800

    Set up for data loading

--- name-of-your-project/src/routes/root.jsx
+++ name-of-your-project/src/routes/root.jsx
@@ -1,7 +1,13 @@
-import { Outlet } from "react-router-dom";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts";
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+}
 
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -30,14 +36,28 @@ export default function Root() {
             </form>
           </div>
           <nav>
+          {contacts.length ? (
             <ul>
-              <li>
-                <Link href={`contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link href={`contacts/2`}>Your Friend</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           </nav>
         </div>
         <div id="detail">
