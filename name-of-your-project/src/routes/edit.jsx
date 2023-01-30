7ed93af14a1b82fe1dfd01089a0d0da402b4b506
Seth Weiss <sweisss@gmail.com>
2023-01-29 20:16:11 -0800

    Hooked up Cancel button

--- name-of-your-project/src/routes/edit.jsx
+++ name-of-your-project/src/routes/edit.jsx
@@ -1,6 +1,7 @@
 import { Form,
     useLoaderData,
     redirect,
+    useNavigate,
 } from "react-router-dom";
 import { updateContact } from "../contacts";
 
@@ -13,6 +14,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const contact = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -62,7 +64,14 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button 
+            type="button"
+            onClick={() => {
+                navigate(-1);
+            }}    
+        >
+            Cancel
+        </button>
       </p>
     </Form>
   );
