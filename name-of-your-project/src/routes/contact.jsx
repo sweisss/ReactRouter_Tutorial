533a09caab35ed3c38c3b78a52e56697b13120c9
Seth Weiss <sweisss@gmail.com>
2023-01-29 20:52:11 -0800

    Added Optimistic UI to favorite button

--- name-of-your-project/src/routes/contact.jsx
+++ name-of-your-project/src/routes/contact.jsx
@@ -80,7 +80,11 @@ export default function Contact() {
 
 function Favorite({ contact }) {
     const fetcher = useFetcher();
+
     let favorite = contact.favorite;
+    if (fetcher.formData) {
+        favorite = fetcher.formData.get("favorite") === "true";
+    }
  
     return (
         <fetcher.Form method="post">
