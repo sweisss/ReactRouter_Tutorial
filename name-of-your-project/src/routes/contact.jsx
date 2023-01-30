f900b7e05d68bef881e4a34edf3750b9738c7dba
Seth Weiss <sweisss@gmail.com>
2023-01-29 20:46:50 -0800

    Favorite button enabled

--- name-of-your-project/src/routes/contact.jsx
+++ name-of-your-project/src/routes/contact.jsx
@@ -1,5 +1,16 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import {
+    Form,
+    useLoaderData,
+    useFetcher,
+} from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+    let formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get("favorite") === "true",
+    });
+}
 
 export async function loader({ params }) {
     return getContact(params.contactId);
@@ -68,21 +79,22 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-  // yes, this is a `let` for later
-  let favorite = contact.favorite;
-  return (
-    <Form method="post">
-      <button
-        name="favorite"
-        value={favorite ? "false" : "true"}
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
-      >
-        {favorite ? "★" : "☆"}
-      </button>
-    </Form>
-  );
+    const fetcher = useFetcher();
+    let favorite = contact.favorite;
+ 
+    return (
+        <fetcher.Form method="post">
+        <button
+            name="favorite"
+            value={favorite ? "false" : "true"}
+            aria-label={
+            favorite
+                ? "Remove from favorites"
+                : "Add to favorites"
+            }
+        >
+            {favorite ? "★" : "☆"}
+        </button>
+        </fetcher.Form>
+    );
 }
